eb1301915b79c6288481a4e053f24c9f8bc21f00
Ricardo Secchin <rick_gava@hotmail.com>
2023-04-03 10:50:59 +0100

    code improvements

--- app/src/main/java/com/picpay/desafio/android/framework/repository/UsersRepository.kt
+++ app/src/main/java/com/picpay/desafio/android/framework/repository/UsersRepository.kt
@@ -2,13 +2,10 @@ package com.picpay.desafio.android.framework.repository
 
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
-import androidx.paging.PagingSource
 import com.picpay.desafio.android.presentation.users.User
 import kotlinx.coroutines.flow.Flow
 
 interface UsersRepository {
-
-    fun getUsers(query: String): PagingSource<Int, User>
 
     fun getCachedUsers(
         query: String,
--- app/src/main/java/com/picpay/desafio/android/framework/repository/UsersRepositoryImpl.kt
+++ app/src/main/java/com/picpay/desafio/android/framework/repository/UsersRepositoryImpl.kt
@@ -2,7 +2,6 @@ package com.picpay.desafio.android.framework.repository
 
 import androidx.paging.*
 import com.picpay.desafio.android.framework.db.AppDatabase
-import com.picpay.desafio.android.framework.paging.UsersPagingSource
 import com.picpay.desafio.android.framework.paging.UsersRemoteMediator
 import com.picpay.desafio.android.framework.repository.remote.UserRemoteDataSource
 import com.picpay.desafio.android.presentation.users.User
@@ -14,10 +13,6 @@ class UsersRepositoryImpl @Inject constructor(
     private val remoteDataSource: UserRemoteDataSource,
     private val userDb: AppDatabase
 ) : UsersRepository {
-
-    override fun getUsers(query: String): PagingSource<Int, User> {
-        return UsersPagingSource(remoteDataSource, query)
-    }
 
     @OptIn(ExperimentalPagingApi::class)
     override fun getCachedUsers(

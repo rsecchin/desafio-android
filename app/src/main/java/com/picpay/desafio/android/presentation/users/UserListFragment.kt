68e8191e8887a77f6b254bfed204b0d01dc52b0d
Ricardo Secchin <rick_gava@hotmail.com>
2023-04-03 12:38:53 +0100

    code improvements

--- app/src/main/java/com/picpay/desafio/android/presentation/users/UserListFragment.kt
+++ app/src/main/java/com/picpay/desafio/android/presentation/users/UserListFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.paging.LoadState
 import com.picpay.desafio.android.databinding.FragmentUserListBinding
 import com.picpay.desafio.android.presentation.users.adapter.UsersAdapter
@@ -22,9 +24,7 @@ class UserListFragment : Fragment() {
 
     private val viewModel: UserListViewModel by viewModels()
 
-    private val userListAdapters by lazy {
-        UsersAdapter()
-    }
+    private lateinit var userListAdapters: UsersAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,18 +39,18 @@ class UserListFragment : Fragment() {
         initUsersAdapter()
         observeInitialLoadState()
 
-        viewModel.state.observe(viewLifecycleOwner) { uiState ->
-            when (uiState) {
-                is UserListViewModel.UiState.SearchResult -> {
-                    userListAdapters.submitData(viewLifecycleOwner.lifecycle, uiState.data)
+        lifecycleScope.launch {
+            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.usersPagingData().collect { pagingData ->
+                    userListAdapters.submitData(pagingData)
                 }
             }
         }
-
-        viewModel.getUsers()
     }
 
     private fun initUsersAdapter() {
+        userListAdapters = UsersAdapter()
+
         postponeEnterTransition()
         with(binding.recyclerUsers) {
             setHasFixedSize(true)
--- app/src/main/java/com/picpay/desafio/android/presentation/users/UserListViewModel.kt
+++ app/src/main/java/com/picpay/desafio/android/presentation/users/UserListViewModel.kt
@@ -1,35 +1,19 @@
 package com.picpay.desafio.android.presentation.users
 
-import androidx.lifecycle.*
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.picpay.desafio.android.framework.usecase.UserUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 @HiltViewModel
 class UserListViewModel @Inject constructor(
     private val useCase: UserUseCase
 ) : ViewModel() {
-
-    private val action = MutableLiveData<Action>()
-    val state: LiveData<UiState> = action
-        .distinctUntilChanged()
-        .switchMap { action ->
-            when (action) {
-                is Action.GetAll -> {
-                    useCase(
-                        UserUseCase.GetUsersParams(getPagingConfig())
-                    ).cachedIn(viewModelScope).map {
-                        UiState.SearchResult(it)
-                    }.asLiveData(Dispatchers.Main)
-                }
-            }
-        }
 
     fun usersPagingData(): Flow<PagingData<User>> {
         return useCase(
@@ -40,16 +24,4 @@ class UserListViewModel @Inject constructor(
     private fun getPagingConfig() = PagingConfig(
         pageSize = 20
     )
-
-    fun getUsers() {
-        action.value = Action.GetAll
-    }
-
-    sealed class UiState {
-        data class SearchResult(val data: PagingData<User>) : UiState()
-    }
-
-    sealed class Action {
-        object GetAll : Action()
-    }
 }
